b8dd76dc165157fab4a1efec49f56ce5dfe66858
flt <flt309386909@163.com>
2019-07-21 11:38:47 +0800

    add save model and jiazai model

--- example/mnist-alexnet.py
+++ example/mnist-alexnet.py
@@ -8,11 +8,13 @@ from tensorflow.examples.tutorials.mnist import input_data
 mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 sess = tf.InteractiveSession()
 
-x = tf.placeholder(tf.float32, [None, 784])
+x = tf.placeholder(tf.float32, [None, 784], name="x-input")
 y_ = tf.placeholder(tf.float32, [None, 10])
 images = tf.reshape(x, [-1,28,28,1])
 
-keep_prob = tf.placeholder(tf.float32)
+tf.summary.image('input_images', images, 10)
+
+keep_prob = tf.placeholder(tf.float32,name='keep_prob')
 
 
 # 在函数inference_op()内定义前向传播的过程
@@ -23,12 +25,14 @@ def inference_op(images,keep_prob):
     with tf.name_scope("conv1"):
         ## 初始化核参数
         kernel = tf.Variable(tf.truncated_normal([2, 2, 1, 96], dtype=tf.float32,stddev=1e-1), name="weights")
+        #tf.contrib.layers.l2_regularizer(0.001)(kernel)
+        #tf.add_to_collection('losses', tf.contrib.layers.l2_regularizer(0.01)(kernel))
         conv = tf.nn.conv2d(images, kernel, [1, 2, 2, 1], padding="SAME")
         ## 初始化偏置
         biases = tf.Variable(tf.constant(0.0, shape=[96], dtype=tf.float32),trainable=True, name="biases")
         conv1 = tf.nn.relu(tf.nn.bias_add(conv, biases))
 
-        conv1 = tf.nn.dropout(conv1, keep_prob)
+        #conv1 = tf.nn.dropout(conv1, keep_prob)
         # 打印第一个卷积层的网络结构
         print(conv1.op.name, ' ', conv1.get_shape().as_list())
 
@@ -44,10 +48,12 @@ def inference_op(images,keep_prob):
     # 在命名空间conv2下实现第二个卷积层
     with tf.name_scope("conv2"):
         kernel = tf.Variable(tf.truncated_normal([2, 2, 96, 256], dtype=tf.float32,stddev=1e-1), name="weights")
+        #tf.contrib.layers.l2_regularizer(0.001)(kernel)
+        #tf.add_to_collection('losses', tf.contrib.layers.l2_regularizer(0.01)(kernel))
         conv = tf.nn.conv2d(pool1, kernel, [1, 1, 1, 1], padding="SAME")
         biases = tf.Variable(tf.constant(0.0, shape=[256], dtype=tf.float32),trainable=True, name="biases")
         conv2 = tf.nn.relu(tf.nn.bias_add(conv, biases))
-        conv12 = tf.nn.dropout(conv2, keep_prob)
+        #conv12 = tf.nn.dropout(conv2, keep_prob)
         parameters += [kernel, biases]
 
         # 打印第二个卷积层的网络结构
@@ -65,11 +71,13 @@ def inference_op(images,keep_prob):
         kernel = tf.Variable(tf.truncated_normal([2,2, 256, 384],
                                                  dtype=tf.float32, stddev=1e-1),
                              name="weights")
+        #tf.contrib.layers.l2_regularizer(0.001)(kernel)
+        #tf.add_to_collection('losses', tf.contrib.layers.l2_regularizer(0.01)(kernel))
         conv = tf.nn.conv2d(pool2, kernel, [1, 1, 1, 1], padding="SAME")
         biases = tf.Variable(tf.constant(0.0, shape=[384], dtype=tf.float32),
                              trainable=True, name="biases")
         conv3 = tf.nn.relu(tf.nn.bias_add(conv, biases))
-        conv3 = tf.nn.dropout(conv3, keep_prob)
+        #conv3 = tf.nn.dropout(conv3, keep_prob)
         parameters += [kernel, biases]
 
         # 打印第三个卷积层的网络结构
@@ -80,11 +88,14 @@ def inference_op(images,keep_prob):
         kernel = tf.Variable(tf.truncated_normal([2, 2, 384, 384],
                                                  dtype=tf.float32, stddev=1e-1),
                              name="weights")
+        #tf.contrib.layers.l2_regularizer(0.001)(kernel)
+        #tf.add_to_collection('losses', tf.contrib.layers.l2_regularizer(0.01)(kernel))
         conv = tf.nn.conv2d(conv3, kernel, [1, 1, 1, 1], padding="SAME")
+
         biases = tf.Variable(tf.constant(0.0, shape=[384], dtype=tf.float32),
                              trainable=True, name="biases")
         conv4 = tf.nn.relu(tf.nn.bias_add(conv, biases))
-        conv4 = tf.nn.dropout(conv4, keep_prob)
+        #conv4 = tf.nn.dropout(conv4, keep_prob)
         parameters += [kernel, biases]
 
         # 打印第四个卷积层的网络结构
@@ -95,12 +106,14 @@ def inference_op(images,keep_prob):
         kernel = tf.Variable(tf.truncated_normal([2, 2, 384, 256],
                                                  dtype=tf.float32, stddev=1e-1),
                              name="weights")
+        #tf.contrib.layers.l2_regularizer(0.001)(kernel)
+        #tf.add_to_collection('losses', tf.contrib.layers.l2_regularizer(0.01)(kernel))
         conv = tf.nn.conv2d(conv4, kernel, [1, 1, 1, 1], padding="SAME")
         biases = tf.Variable(tf.constant(0.0, shape=[256], dtype=tf.float32),
                              trainable=True, name="biases")
 
         conv5 = tf.nn.relu(tf.nn.bias_add(conv, biases))
-        conv5 = tf.nn.dropout(conv5, keep_prob)
+        #conv5 = tf.nn.dropout(conv5, keep_prob)
         parameters += [kernel, biases]
 
         # 打印第五个卷积层的网络结构
@@ -145,40 +158,75 @@ def inference_op(images,keep_prob):
         # 打印第二个全连接层的网络结构信息
         print(fc_2.op.name, ' ', fc_2.get_shape().as_list())
 
+    # with tf.name_scope("fc_3"):
+    #     fc3_weights = tf.Variable(tf.truncated_normal([2048, 10], dtype=tf.float32,
+    #                                                   stddev=1e-1), name="weights")
+    #     fc3_bias = tf.Variable(tf.constant(0.0, shape=[10],
+    #                                        dtype=tf.float32), trainable=True, name="biases")
+    #     fc_3 = tf.add(tf.matmul(fc_2, fc3_weights),fc3_bias)
+    #     fc_3 = tf.nn.dropout(fc_3, keep_prob)
+    #     parameters += [fc3_weights, fc3_bias]
+
+    #     # 打印第二个全连接层的网络结构信息
+    #     print(fc_3.op.name, ' ', fc_3.get_shape().as_list())
+
     y_conv = tf.nn.softmax(fc_2)
 
     # 返回全连接层处理的结果
     return y_conv, parameters
 
-y_conv, parameters = inference_op(images,keep_prob)
+#global_step = tf.Variable(0)
+#learning_rate = tf.train.exponential_decay(0.8, global_step, 100, 0.1, staircase=True)
 
-print (parameters)
+y_conv, parameters = inference_op(images,keep_prob)
+tf.add_to_collection('pred', y_conv)
+#print (parameters)
 # 计算代价函数
-cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y_conv), reduction_indices=[1]))
+cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y_conv+1e-10), reduction_indices=[1]))
 # 使用AdamOptimizer进行优化
-train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
+#tf.add_to_collection('losses', cross_entropy)
+#loss = tf.add_n(tf.get_collection('losses'))
+tf.summary.scalar('loss', cross_entropy)
+train_step = tf.train.AdagradOptimizer(1e-3).minimize(cross_entropy)
 
 # 计算准确率
 correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+tf.summary.scalar('accuracy', accuracy)
 
+
+
+# 写到指定的磁盘路径中
+
+from datetime import datetime
+TIMESTAMP = "{0:%Y-%m-%dT%H-%M-%S/}".format(datetime.now())
+train_log_dir = 'tensorboard/train/' + TIMESTAMP
+writer_train = tf.summary.FileWriter(train_log_dir,sess.graph)
+
+saver = tf.train.Saver() 
+merged = tf.summary.merge_all()
 # 初始化全局变量
 tf.global_variables_initializer().run()
 
 
-for i in range(20000):
+##训练过程
+
+for i in range(25000):
     batch = mnist.train.next_batch(100)
     if i % 100 == 0:
-        train_accuracy = accuracy.eval(feed_dict={x: batch[0], y_: batch[1],keep_prob:0.75})
-        print('step %d, training accuracy %g' % (i, train_accuracy))
+        train_accuracy = accuracy.eval(feed_dict={x: batch[0], y_: batch[1],keep_prob:0.9})
+        #print('step %d, training accuracy %g' % (i, train_accuracy))
+        _, loss_value = sess.run([train_step, cross_entropy],feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.9})
+        print ('After step %d, training accuracy %g' % (i, train_accuracy),'training loss is %f'%(loss_value))
+        summary, _ = sess.run([merged, train_step],feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.9})
+        writer_train.add_summary(summary, i)
         #cross_entropy=sess.run(train_step,feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.75})
         #print ('step %d, loss %f'%(i, cross_entropy))
     #loss=sess.run(train_step,feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
     #print (loss)
-    train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
+    #train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
 
+saver.save(sess, "Model_save/mnist-alexnet-model3.ckpt")
 
-
+##测试过程
 print('test accuracy %g' % accuracy.eval(feed_dict={x: mnist.test.images, y_: mnist.test.labels,keep_prob:1}))
-
-
